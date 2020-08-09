d58c0bb77a60326a6c78bd9e361c4b693f98f621
alexhokl <alexhokl@gmail.com>
2020-08-09 15:25:39 +0800

    Updated github.com/alexhokl/database

--- command/screen.go
+++ command/screen.go
@@ -2,11 +2,6 @@ package command
 
 import (
 	"errors"
-	"fmt"
-	"os"
-	"strings"
-	"text/tabwriter"
-	"time"
 
 	"github.com/alexhokl/database"
 	"github.com/alexhokl/go-sql-export/model"
@@ -17,6 +12,7 @@ type screenOption struct {
 	configOption
 }
 
+// NewScreenCommand returns a command
 func NewScreenCommand(cli *ManagerCli) *cobra.Command {
 	opts := screenOption{}
 
@@ -61,46 +57,7 @@ func runScreen(config *model.ExportConfig) error {
 		dataList = append(dataList, *data)
 	}
 
-	printDataList(dataList)
+	database.DumpTables(dataList)
 
 	return nil
-}
-
-func printDataList(list []database.TableData) error {
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', tabwriter.AlignRight)
-	for _, data := range list {
-		fmt.Fprintf(w, "%s\t\n", strings.Join(data.Columns, "\t"))
-		for _, r := range data.Rows {
-			vals := getStringValues(r)
-			fmt.Fprintf(w, "%s\t\n", strings.Join(vals, "\t"))
-		}
-		w.Flush()
-	}
-	return nil
-}
-
-func getStringValues(row []interface{}) []string {
-	list := []string{}
-	for _, c := range row {
-		list = append(list, getStringValue(c.(*interface{})))
-	}
-	return list
-}
-
-func getStringValue(val *interface{}) string {
-	switch v := (*val).(type) {
-	case nil:
-		return "NULL"
-	case bool:
-		if v {
-			return "TRUE"
-		}
-		return "FALSE"
-	case []byte:
-		return string(v)
-	case time.Time:
-		return v.Format("2006-01-02 15:04:05.999")
-	default:
-		return fmt.Sprint(v)
-	}
 }
